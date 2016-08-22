d846513ebb751ae53ca9533052a0c4eab454e539
Karl Steltenpohl <karl@webksd.com>
2016-08-22 16:07:55 -0500

    kjs

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
 $(function() {
 
   // Body Click...
-  $('body').on('click','[data-toggle="collapse"]',function(e) {
+  $('body')
+  // collapse
+  .on('click','[data-toggle="collapse"]',function(e) {
     if($(e.target).hasClass('glyphicon-minus')) {
       $(e.target).removeClass('glyphicon-minus').addClass('glyphicon-plus');
     } else {
       $(e.target).removeClass('glyphicon-plus').addClass('glyphicon-minus');
     }
+  })
+  // add row
+  .on('click','.add-row',function(e){
+    var cloneTarget = $(e.target).attr('data-target');
+
   });
 
-  // Date Time Picker
-  $('.datepicker').datetimepicker();
+  // Date Picker
+  $('.datepicker').datetimepicker({
+    format : 'MM/DD/YYYY'
+  });
+
+  // Time Picker
+  $('.timepicker').datetimepicker();
+
+  // Date + Time Picker
+  $('.datetimepicker').datetimepicker({
+    sideBySide: true
+  });
 
   // Select Menu
   $('.selectpicker').selectpicker();
+
+  //
 
 });
--- readme.md
+++ readme.md
@@ -8,8 +8,21 @@
 Begin by running `npm install` and `bower install` to update
 node and frontend dependencies and make sure everything is working.
 
-Also make sure you have the grunt client installed.
+# Requirements
+- Node + npm
+- Sass
+- Ruby
+- Grunt cli
 
+# Dev
 Then just run `grunt` to preprocess css and serve the site locally.
 
+# Distribution
 To process the site for distribution run grunt copy:dist after running and stopping a grunt cycle.
+
+# Changelog
+- 08/22/16 | Date and Time picker updates, Button updates 
+- 08/**/16 | Built Form
+- 07/**/16 | Built Footer
+- 07/**/16 | Built Header 
+- 07/**/16 | Built tables
